b22ddb4778de68d74ffd9c5051d8ffc3a15aa6e5
ariel <ariel.f2002@gmail.com>
2018-06-20 23:22:38 +0300

    now can search every kind of post, not just [cards]

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-import praw , discord
+import praw
+from discord.ext import commands
 import os # I need this to use environment variables on heroku
 import csv # this is to browse the core set
-from discord.ext import commands
 
 # Reddit variables
 reddit = praw.Reddit(client_id = 'x4FICJQqO4D14g' , client_secret = 'i9kip94Qs6R4Kfy77XYzDuv0z8Q' , user_agent = 'Card fetcher for collective.') # gives access to reddit
@@ -9,7 +9,8 @@ collective = reddit.subreddit('collectivecg') # gives access to the Collective s
 
 # discord variables
 bot = commands.Bot(command_prefix='?')
-
+core_set = {} # here is the core_set saved
+temp_cards = {}
 # functions
 def get_card_name(text):
     '''takes a striing and extracts card names from it. card names are encapsulated in [[xxxx]] where xxxx is the card name'''
@@ -26,9 +27,25 @@ def get_card_name(text):
     return cards
 
 def save_card(name , link):
+    '''saves a link to a card picture with the name specified in the temp_cards.csv document'''
     with open('temp_cards.csv' , 'a') as temp_cards_file:
         temp_cards = csv.writer(temp_cards_file , delimiter = ',')
         temp_cards.writerow([name , link])
+
+def load_core_set(core_set):
+    '''loads the core set cards from core_set.csv into a dictionary called core_set'''
+    with open('core_set.csv' , 'r') as core_set_file:
+        core_set_csv = csv.reader(core_set_file , delimeter = ',')
+        for row in core_set_csv:
+            name , link = row # unpack the row into a name and a link
+            core_set[name] = link # adds the card into the core_set dictionary
+def load_temp_cards(temp_cards):
+    '''loads the temp saved cards from temp_cards.csv into a dictionary called temp_cards'''
+    with open('core_set.csv' , 'r') as temp_cards_file:
+        temp_cards_csv = csv.reader(temp_cards_file , delimeter = ',')
+        for row in temp_cards_csv:
+            name , link = row # unpack the row into a name and a link
+            temp_cards[name] = link # adds the card into the core_set dictionary
 # events
 @bot.event
 async def on_message(message):
@@ -36,6 +53,7 @@ async def on_message(message):
         parameters = message.content.split(' ') # all commands look like this : '!command par1 par2 par3...'
         if parameters[0] == '!save':
             save_card(parameters[1] , parameters[2]) # saved the card data (name , link) into the csv with temp cards.
+            temp_cards[parameters[1]] = parameters[2] # adds the card to the current dictionary
             await bot.send_message(message.channel , '{} was added!'.format(parameters[1])) # sends confirmation message
             return 0 # quits the function
     else:
@@ -50,24 +68,19 @@ async def on_message(message):
                         links.append(post.url)
             else:
                 found = False
-                for post in collective.search('[card] {}'.format(card) , limit = 1): # this searches the subreddit for the card name with the [card] tag and takes the top suggestion
-                    found = True
-                    links.append(post.url)
-                if not found: # if we didnt find any cards that go by that name
-                    with open('core_set.csv' , 'r') as core_set_file: # the we check in the core_set
-                        with open('temp_cards.csv' , 'r') as temp_cards_file:
-                            core_set = csv.reader(core_set_file , delimiter = ',') # this opens the csv file
-                            temp_cards = csv.reader(temp_cards_file , delimiter = ',')
-                            for csv_file in [core_set , temp_cards]: # runs trough both files
-                                for core_card in csv_file: # this runs trough all the rows
-                                    name , link = core_card
-                                    if name.lower() == card.lower():
-                                        links.append(link)
-                                        found = True
-                                    if found:
-                                        break # no need to continue searching
-                                if found:
-                                    break
+                for post in collective.search(card , limit = 1): # this searches the subreddit for the card name with the [card] tag and takes the top suggestion
+                    if post.title.startswith('[') and not (post.title.startswith('[Update]') and not post.title.startswith('[Card][Update]')):
+                        links.append(post.url)
+                        found = True
+                if not found: # if we didn't find any cards that go by that name
+                    if card in core_set:
+                        links.append(core_set[card])
+                    elif card in temp_cards:
+                        links.append(message.channel, temp_cards[card])
         for x in range((len(links)//5)+1): # this loops runs one time plus once for every five links since discord can only display five pictures per message
             await bot.send_message(message.channel , '\n'.join(links[5*x:5*(x+1)]))
+
+#main
+load_core_set(core_set)
+load_temp_cards(temp_cards)
 bot.run(os.environ.get('BOT_TOKEN'))
