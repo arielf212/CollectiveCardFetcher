cfd7c647e4f57b4d1da27bb834adbd3bc4b58bcb
ariel <ariel.f2002@gmail.com>
2018-08-03 03:56:27 +0300

    rewrote a big part of the bot. added a help command. all of the commands are now actual commands and not me parsing the message. you can now add "core:" before a card name to only search in core set

--- bot.py
+++ bot.py
@@ -1,19 +1,26 @@
 import praw
 from discord.ext import commands
+import discord
 from fuzzywuzzy import fuzz
 import asyncio
 import os # I need this to use environment variables on heroku
 import csv # this is to browse the core set
+
 # Reddit variables
 reddit = praw.Reddit(client_id = 'x4FICJQqO4D14g' , client_secret = 'i9kip94Qs6R4Kfy77XYzDuv0z8Q' , user_agent = 'Card fetcher for Collective.') # gives access to reddit
 collective = reddit.subreddit('collectivecg') # gives access to the Collective subreddit
 
 # discord variables
-bot = commands.Bot(command_prefix='?')
-
-#regular variables
-global post
-post = False
+bot = commands.Bot(command_prefix='!')
+embed = discord.Embed(title = "CollectiveCardFetcher Help", description = "here is a list of commands for the bot:" , color= 0x00FFFF)
+embed.add_field(name = '!alive', value = 'The bot will respond with "im alive and well!"')
+embed.add_field(name = '!server', value = "The bot will respond with a link that can be used to add him to a server. note: you need to be an admin in a server to add a bot.")
+embed.add_field(name = '!github', value = "The bot will respond with a link to the github page of the bot.")
+embed.add_field(name = '!nice' , value = 'The bot will respond with a "nice art!" picture.')
+embed.add_field(name = '!good' , value = 'Ups the score of the bot. Will make the bot respond with a thankful message.')
+embed.add_field(name = '!bad', value = 'Reduces the score of the bot. Will make the bot respond with an apologetic message.')
+embed.add_field(name = '!score' , value = 'the bot will respond with the amount of votes given to him trough !bad and !good')
+bot.remove_command('help')
 
 # functions
 def get_card_name(text):
@@ -25,7 +32,16 @@ def get_card_name(text):
         if end == -1:
             return cards # if there is an opener but no closer then someone fucked up
         else:
-            cards.append(text[start+2:end]) # gets the name of the card
+            query = text[start + 2:end]
+            if query.find(':') > 0:
+                mod = query[:query.find(':')].lower()
+                card = query[query.find(':')+1:]
+                if mod not in ['hs','eternal','mtg','core','reddit','dc','ygo','sb']:
+                    card = query
+                    mod = 'none'
+                cards.append((mod,card))
+            else:
+                cards.append(('none',query)) # gets the name of the card
         text = text[end+2 : ] # cuts out the part with the card
         start = text.find('[[') # and the circle begins anew
     return cards
@@ -101,77 +117,82 @@ def get_top(num , week):
     return_list = []
     index = 1
     while index <= num:
-        post = ''
-        for submission in collective.search('flair:(week ' + str(week) + ')',limit = index , sort = 'top'):
-            post = submission
-        if post.title.startswith('[Cosmetic') or post.title.startswith('[DC'):
-            num+=1
-            index+=1
-        else:
+        post = list(collective.search('flair:(week ' + str(week) + ')',limit = index , sort = 'top'))[-1]
+        if post.title.startswith('[Card') or post.title.startswith('[Update'):
             return_list.append(post.url)
             index+=1
+        else:
+            num+=1
+            index+=1
     return return_list
+
+# commands
+@bot.command()
+async def alive():
+    await bot.say('im alive and well!')
+
+@bot.command()
+async def server():
+    await bot.say('https://discordapp.com/api/oauth2/authorize?client_id=465866501715525633&permissions=522304&scope=bot')
+
+@bot.command()
+async def github():
+    await bot.say('https://github.com/fireasembler/CollectiveCardFetcher')
+
+@bot.command(pass_context=True)
+async def nice(ctx):
+    await bot.send_file(ctx.message.channel, 'nice art.jpg')
+
+@bot.command()
+async def good():
+    os.environ['GOOD'] = str(int(os.environ['GOOD']) + 1)
+    await bot.say('thank you! :)')
+
+@bot.command()
+async def bad():
+    os.environ['BAD'] = str(int(os.environ['BAD']) + 1)
+    await bot.say('ill try better next time :(')
+
+@bot.command()
+async def score():
+    await bot.say('good: ' + os.environ.get('GOOD'))
+    await bot.say('bad: ' + os.environ.get('BAD'))
+
+@bot.command()
+async def help():
+    await bot.say(embed=embed)
 
 # events
 @bot.event
 async def on_message(message):
-    global post
-    if message.content.startswith('!'):
-        parameters = message.content.split(' ') # all commands look like this : '!command par1 par2 par3...'
-        if parameters[0] == '!save':
-            save_card(' '.join(parameters[1:-1]), parameters[-1]) # saved the card data (name , link) into the csv with temp cards.
-            temp_cards[parameters[1]] = parameters[2] # adds the card to the current dictionary
-            await bot.send_message(message.channel , '{} was added!'.format(parameters[1])) # sends confirmation message
-            return 0 # quits the function
-        elif parameters[0] == '!alive':
-            await bot.send_message(message.channel , 'im alive and well!')
-        elif parameters[0] == '!server':
-            await bot.send_message(message.channel , 'https://discordapp.com/api/oauth2/authorize?client_id=465866501715525633&permissions=522304&scope=bot')
-        elif parameters[0] == '!github' or parameters[0] == '!code':
-            await bot.send_message(message.channel , 'https://github.com/fireasembler/CollectiveCardFetcher')
-        elif parameters[0] == '!start':
-            post = True
-            await bot.send_message(message.channel, 'will start posting here!')
-            bot.loop.create_task(repost_card(message.channel))
-        elif parameters[0] == '!stop':
-            post = False
-            await bot.send_message(message.channel, "will stop!")
-        elif parameters[0] == '!nice' and parameters[1] in ['art', 'art!']:
-            await bot.send_file(message.channel , 'nice art.jpg')
-        elif parameters[0].startswith('!good'):
-            os.environ['GOOD'] = str(int(os.environ['GOOD']) + 1)
-            await bot.send_message(message.channel , 'thank you! :)')
-        elif parameters[0].startswith('!bad'):
-            os.environ['BAD'] = str(int(os.environ['BAD']) + 1)
-            await bot.send_message(message.channel , 'ill try better next time :(')
-        elif parameters[0] == '!score':
-            await bot.send_message(message.channel , 'good: ' + os.environ.get('GOOD'))
-            await bot.send_message(message.channel ,'bad: ' + os.environ.get('BAD'))
-    else:
-        cards = get_card_name(message.content) # this gets all card names in the message
-        links = [] # here are the card links stored
-        for card in cards:
-            if card.startswith('top '):
-                if len(card.split(' ')) == 2: # the name looks like this :"top X"
-                    num = card.split(' ')[1]
-                    if num.isdigit():
-                        links += get_top(int(num) , os.environ.get('WEEK'))
-                elif len(card.split(' ')) == 4 and card.split(' ')[2] == 'week': # the name looks like this: "top X week Y"
-                    num = card.split(' ')[1]
-                    week = card.split(' ')[3]
-                    if num.isdigit() and week.isdigit():
-                        links += get_top(int(num) , int(week))
-            else:
-                found = False
-                for post in collective.search(card , limit = 1): # this searches the subreddit for the card name with the [card] tag and takes the top suggestion
-                    if post.title.startswith('[Card]') or post.title.startswith('[DC') or post.title.startswith('[Meta]'):
+    cards = get_card_name(message.content) # this gets all card names in the message
+    links = [] # here are the card links stored
+    for card in cards:
+        mod , card = card
+        if card.lower().startswith('top '):
+            if len(card.split(' ')) == 2: # the name looks like this :"top X"
+                num = card.split(' ')[1]
+                if num.isdigit():
+                    links += get_top(int(num) , os.environ.get('WEEK'))
+            elif len(card.split(' ')) == 4 and card.split(' ')[2] == 'week': # the name looks like this: "top X week Y"
+                num = card.split(' ')[1]
+                week = card.split(' ')[3]
+                if num.isdigit() and week.isdigit():
+                    links += get_top(int(num) , int(week))
+        else:
+            found = False
+            print(mod)
+            if mod == 'none':
+                for post in collective.search(card , limit = 1, sort= 'new'): # this searches the subreddit for the card name with the [card] tag and takes the top suggestion
+                    if post.title.startswith('['):
                         links.append(post.url)
                         found = True
-                if not found: # if we didn't find any cards that go by that name
-                    links.append(get_link(card))
-        if links: # if there are any links
-            for x in range((len(links)//5)+1): # this loops runs one time plus once for every five links since discord can only display five pictures per message
-                await bot.send_message(message.channel , '\n'.join(links[5*x:5*(x+1)]))
+            if not found: # if we didn't find any cards that go by that name
+                links.append(get_link(card))
+    if links: # if there are any links
+        for x in range((len(links)//5)+1): # this loops runs one time plus once for every five links since discord can only display five pictures per message
+            await bot.send_message(message.channel , '\n'.join(links[5*x:5*(x+1)]))
+    await bot.process_commands(message)
 
 #main
 core_set = load_core_set()
